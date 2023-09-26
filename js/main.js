67ec88c784a56575aff4423fa0825c07e37af40f
Gia Cuong <legiacuong789@gmai.com>
2023-09-26 11:05:03 +0700

    Done updating products onto localStorage

--- js/main.js
+++ js/main.js
@@ -1,36 +1,25 @@
-const products = [
-          {
-                    id: 0,
-                    image: "../asset/image/apple-watch.png",
-                    title: "Apple watch",
-                    price: 120,
-          },
-          {
-                    id: 1,
-                    image: "../asset/image/ipone-11.png",
-                    title: "Iphone 11",
-                    price: 200,
-          },
-          {
-                    id: 2,
-                    image: "../asset/image/airpods.png",
-                    title: "Airpods",
-                    price: 50,
-          },
-          {
-                    id: 3,
-                    image: "../asset/image/charger.png",
-                    title: "Charger",
-                    price: 30,
-          },
-]
+fetch("../js/products1.json")
+          .then(response => {
+                    return response.json() // get products from json file and parse to javascript
+          })
+          .then(data => {
+                    localStorage.setItem("products", JSON.stringify(data))
+                    if (!localStorage.getItem("cart")) {
+                              localStorage.setItem("cart", "[]")
+                    }
+          })
+          .catch(error => {
+                    console.log(error)
+          })
 
+// Get products and cart from localStorage
+const products = JSON.parse(localStorage.getItem("products"))
+let cart = JSON.parse(localStorage.getItem("cart"))
 const categories = [...new Set(products.map(item => item))]
 
 // Render products 
-let i = 0
 document.getElementById('root').innerHTML = categories.map(item => {
-          var { image, title, price } = item
+          var { id, image, title, price } = item // destructor (ES6)
           return (
                     `<div class='box'>
                               <div class='image-box'>
@@ -38,54 +27,98 @@ document.getElementById('root').innerHTML = categories.map(item => {
                               </div>
                               <div class='bottom'>
                                         <p>${title}</p>
-                                        <h2>${price}.00</h2>` +
-                    "<button type='button' onclick='addToCart(" + (i++) + ")'>Add to cart</button>" +
-                    `</div>
-                    </div> `
+                                        <h2>${price}.00</h2>
+                                        <button type='button' onclick='addToCart(${id} )'>Add to cart</button>
+                              </div >
+                    </div > `
           )
 }).join('')
 
-// cart to accomodate items
-var cart = []
+// Add the product to the cart
+function addItemToCart(productId) {
+          const product = products.find(product => {
+                    return product.id === productId
+          })
+
+          if (!product) {
+                    console.error("Product not found!")
+                    return;
+          }
+
+          // If the product exists in the cart, then set quantity for the product is 1. If yes, increasing one
+          const existingItem = cart.find(item => {
+                    return item.id === productId
+          })
+
+          if (!existingItem) {
+                    cart.push({ ...product, quantity: 1 }) // the quantity of product will be overrided
+          } else {
+                    existingItem.quantity++
+          }
+
+          localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+// Delete a product from the cart
+function deleteItemFromCart(productId) {
+          // Get all products and update cart array except the product wants to delete 
+          cart = cart.filter(product => {
+                    return product.id !== productId
+          })
+
+          localStorage.setItem("cart", JSON.stringify(cart))
+}
 
 // add to cart
-addToCart = (a) => {
-          cart.push({ ...categories[a] })
+addToCart = (productId) => {
+          addItemToCart(productId)
           displayCart()
 }
 
 // delete from cart
-deleteItem = (a) => {
-          cart.splice(a, 1)
+deleteItem = (index) => {
+          const productId = cart[index].id
+          deleteItemFromCart(productId)
           displayCart()
 }
 
 // refresh and display the updated cart
 displayCart = () => {
-          let j = 0
-          let total = 0
+          let totalPrice = 0
+          let totalQuantity = cart.reduce((accumulate, products) => {
+                    return accumulate + products.quantity
+          }, 0)
 
-          document.getElementById('count').innerHTML = cart.length
+          const cartContainer = document.getElementById('cart-item')
+
+          document.getElementById('count').innerHTML = totalQuantity
 
           if (cart.length === 0) {
                     document.getElementById('cart-item').innerHTML = "Your cart is empty"
-                    document.getElementById('total').innerHTML = '$ ' + 0 + '.00'
           } else {
-                    document.getElementById('cart-item').innerHTML = cart.map(items => {
-                              var { image, title, price } = items
+                    cartContainer.innerHTML = "" // clear the previous content before updating new contents
 
-                              total += price
-                              document.getElementById('total').innerHTML = '$ ' + total + '.00'
+                    cart.forEach((item, index) => {
+                              const { image, title, price, quantity } = item
 
-                              return (
-                                        `<div class='cart-item-'>
-                                                  <div class='row-img'>
+                              totalPrice += price * quantity
+                              document.getElementById('total').innerHTML = '$ ' + totalPrice + '.00'
+
+                              const cartDiv = document.createElement("div")
+                              cartDiv.className = "cart-item-"
+                              cartDiv.innerHTML = `<div class='row-img'>
                                                             <img class='rowimg' src=${image}></img>
                                                   </div>
                                                   <p style='font-size: 13px;'>${title}</p>
-                                                  <h2 style='font-size: 15px;'>${price}.00</h2>` +
-                                        "<i class='fa-solid fa-trash' onclick='deleteItem(" + (j++) + ")'></i></div>"
-                              )
-                    }).join('')
+                                                  <h2 style='font-size: 15px;'>${price}.00</h2>
+                                                  <p style='font-size: 16px; font-style: italic;'>(${quantity})</p>
+                                                  <i class='fa-solid fa-trash' onclick='deleteItem(${index})'></i>
+                                                  `
+
+                              cartContainer.appendChild(cartDiv)
+                    })
           }
 }
+
+// Call displayCart to initially populate the cart
+displayCart()
